43b14941b72d4097f94af73de2e18dad1b94c86c
eloueriemmi <eloueriemmi@gmail.com>
2026-01-23 22:30:28 +0100

    Add proper HTTP headers to student service

--- frontend/src/app/services/students.service.ts
+++ frontend/src/app/services/students.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { environment } from '../../environments/environment';
 
@@ -7,7 +7,7 @@ export interface Student {
   id?: number;
   nom: string;
   prenom: string;
-  email: string;
+  email?: string;
   telephone?: string;
   dateNaissance?: string;
   adresse?: string;
@@ -28,9 +28,15 @@ export interface Student {
   providedIn: 'root'
 })
 export class StudentsService {
- private apiUrl = `${environment.apiUrl}/students`;
+  private apiUrl = `${environment.apiUrl}/students`;
 
   constructor(private http: HttpClient) {}
+
+  private getHeaders(): HttpHeaders {
+    return new HttpHeaders({
+      'Content-Type': 'application/json'
+    });
+  }
 
   getAll(): Observable<Student[]> {
     return this.http.get<Student[]>(this.apiUrl);
@@ -41,11 +47,17 @@ export class StudentsService {
   }
 
   create(student: Student): Observable<Student> {
-    return this.http.post<Student>(this.apiUrl, student);
+    console.log('Service create() - Envoi:', student);
+    return this.http.post<Student>(this.apiUrl, student, {
+      headers: this.getHeaders()
+    });
   }
 
   update(id: number, student: Partial<Student>): Observable<Student> {
-    return this.http.patch<Student>(`${this.apiUrl}/${id}`, student);
+    console.log('Service update() - Envoi:', student);
+    return this.http.patch<Student>(`${this.apiUrl}/${id}`, student, {
+      headers: this.getHeaders()
+    });
   }
 
   delete(id: number): Observable<void> {
