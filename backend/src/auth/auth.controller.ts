f4c7d9757875eb5c6928b932d8279e0662e534c2
eloueriemmi <eloueriemmi@gmail.com>
2026-01-28 19:21:51 +0100

    Backup avant retour à a4aa30d

--- backend/src/auth/auth.controller.ts
+++ backend/src/auth/auth.controller.ts
@@ -13,7 +13,7 @@ export class AuthController {
   }
 
   @Post('login')
-  async login(@Body(ValidationPipe) loginDto: LoginDto) {
+  async login(@Body() loginDto: LoginDto) {
     return this.authService.login(loginDto);
   }
 }
